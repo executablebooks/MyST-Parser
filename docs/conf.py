4bcce5eeb8282aa152604a1eff0631b5d76bf1fd
Chris Holdgraf <choldgraf@berkeley.edu>
2020-05-04 16:07:31 -0700

    using sphinx book theme docs (#129)

--- docs/conf.py
+++ docs/conf.py
@@ -50,10 +50,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_book_theme"
 html_logo = "_static/logo.png"
 html_theme_options = {
-    "github_url": "https://github.com/ExecutableBookProject/MyST-Parser"
+    "github_url": "https://github.com/ExecutableBookProject/MyST-Parser",
+    "repository_url": "https://github.com/ExecutableBookProject/MyST-Parser",
+    "expand_sections": ["using/index"],
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
