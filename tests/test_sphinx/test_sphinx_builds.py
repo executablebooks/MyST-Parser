73b50e3e019f73b3bd10bfa64463c7946745d8c8
Chris Sewell <chrisj_sewell@hotmail.com>
2020-08-31 11:52:49 +0100

    Update test_sphinx_builds.py

--- tests/test_sphinx/test_sphinx_builds.py
+++ tests/test_sphinx/test_sphinx_builds.py
@@ -113,6 +113,9 @@ def test_includes(
                 # r"'subfolder\\example2.jpg'": "'subfolder/example2.jpg'",
                 # r"'subfolder\\\\example2.jpg'": "'subfolder/example2.jpg'",
                 # r'uri="subfolder\\example2.jpg"': 'uri="subfolder/example2.jpg"',
+                r"'subfolder\\example2.jpg'}\" uri=\"subfolder\\example2.jpg\"": (
+                    r"'subfolder/example2.jpg'}\" uri=\"subfolder/example2.jpg\""
+                )
             },
         )
     finally:
