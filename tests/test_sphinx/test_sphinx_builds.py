dbf9facfe7f7d3b516f9beea99eb85881e0a8848
Chris Sewell <chrisj_sewell@hotmail.com>
2020-08-31 12:07:37 +0100

    Update test_sphinx_builds.py

--- tests/test_sphinx/test_sphinx_builds.py
+++ tests/test_sphinx/test_sphinx_builds.py
@@ -110,15 +110,18 @@ def test_includes(
             regress=True,
             # fix for Windows CI
             replace={
-                # r"'subfolder\\example2.jpg'": "'subfolder/example2.jpg'",
+                r"subfolder\\example2.jpg": "subfolder/example2.jpg",
+                r"subfolder\\\\example2.jpg": "subfolder/example2.jpg",
+                r"subfolder\\\\\\example2.jpg": "subfolder/example2.jpg",
+                r"subfolder\\\\\\\\example2.jpg": "subfolder/example2.jpg",
                 # r"'subfolder\\\\example2.jpg'": "'subfolder/example2.jpg'",
                 # r'uri="subfolder\\example2.jpg"': 'uri="subfolder/example2.jpg"',
-                r"'subfolder\\example2.jpg'}\" uri=\"subfolder\\example2.jpg\"": (
-                    r"'subfolder/example2.jpg'}\" uri=\"subfolder/example2.jpg\""
-                ),
-                r"'subfolder\\\\example2.jpg'}\" uri=\"subfolder\\example2.jpg\"": (
-                    r"'subfolder/example2.jpg'}\" uri=\"subfolder/example2.jpg\""
-                ),
+                # r"'subfolder\\example2.jpg'}\" uri=\"subfolder\\example2.jpg\"": (
+                #     r"'subfolder/example2.jpg'}\" uri=\"subfolder/example2.jpg\""
+                # ),
+                # r"'subfolder\\\\example2.jpg'}\" uri=\"subfolder\\example2.jpg\"": (
+                #     r"'subfolder/example2.jpg'}\" uri=\"subfolder/example2.jpg\""
+                # ),
             },
         )
     finally:
