45096a19ee57ce13e20cffa12f45ebf2649fc7b0
Chris Sewell <chrisj_sewell@hotmail.com>
2021-01-05 16:50:44 +0000

    Update myst_parser/main.py

--- myst_parser/main.py
+++ myst_parser/main.py
@@ -98,7 +98,7 @@ class MdParserConfig:
     substitutions: Dict[str, str] = attr.ib(
         factory=dict,
         validator=deep_mapping(instance_of(str), instance_of(str), instance_of(dict)),
-        repr=lambda v: str(list(v))
+        repr=lambda v: str(list(v)),
     )
 
     sub_delimiters: Tuple[str, str] = attr.ib(default=("{", "}"))
