5f37c0b24b8b0079d510d79cceb4257396fee708
Chris Holdgraf <choldgraf@berkeley.edu>
2021-06-09 12:53:31 -0700

    📚 DOCS: Updating logos (#386)

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_book_theme"
-html_logo = "_static/logo.png"
+html_logo = "_static/logo-wide.svg"
+html_favicon = "_static/logo-square.svg"
 html_title = ""
 html_theme_options = {
     "github_url": "https://github.com/executablebooks/MyST-Parser",
