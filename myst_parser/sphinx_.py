0b5d3c956016068d2bffc3ab300dc85348af241a
Chris Sewell <chrisj_sewell@hotmail.com>
2021-08-22 09:09:27 +0200

    🔧 MAINTAIN: Support include parser option

---
+++ myst_parser/sphinx_.py
@@ -0,0 +1,6 @@
+"""A module for compatibility with the docutils>=0.17 `include` directive, in RST documents::
+
+   .. include::
+      :parser: myst_parser.sphinx_
+"""
+from myst_parser.sphinx_parser import MystParser as Parser  # noqa: F401
