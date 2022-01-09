e0cf7da338f4c345f9f4e2dc490270c8db427020
Chris Sewell <chrisj_sewell@hotmail.com>
2022-01-09 09:34:57 +0100

    🧪 TESTS: Use `pytest-param-files` (#496)
    I created https://pypi.org/project/pytest_param_files/,
    to simplify the fixture based testing 😄

--- tests/test_renderers/test_fixtures_sphinx.py
+++ tests/test_renderers/test_fixtures_sphinx.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import pytest
 import sphinx
-from markdown_it.utils import read_fixture_file
+from pytest_param_files import with_parameters
 
 from myst_parser.main import MdParserConfig, to_docutils
 from myst_parser.sphinx_renderer import SphinxRenderer, mock_sphinx_env
@@ -20,163 +20,84 @@ def test_minimal_sphinx():
         assert app.config["author"] == "bob geldof"
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("sphinx_syntax_elements.md")),
-    ids=[
-        f"{i[0]}-{i[1]}"
-        for i in read_fixture_file(FIXTURE_PATH / "sphinx_syntax_elements.md")
-    ],
-)
-def test_syntax_elements(line, title, input, expected):
-    document = to_docutils(input, in_sphinx_env=True)
-    print(document.pformat())
-    assert "\n".join(
-        [ll.rstrip() for ll in document.pformat().splitlines()]
-    ) == "\n".join([ll.rstrip() for ll in expected.splitlines()])
+@with_parameters(FIXTURE_PATH / "sphinx_syntax_elements.md")
+def test_syntax_elements(file_params):
+    document = to_docutils(file_params.content, in_sphinx_env=True)
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("tables.md")),
-    ids=[f"{i[0]}-{i[1]}" for i in read_fixture_file(FIXTURE_PATH / "tables.md")],
-)
-def test_tables(line, title, input, expected):
-    document = to_docutils(input, in_sphinx_env=True)
-    print(document.pformat())
-    assert "\n".join(
-        [ll.rstrip() for ll in document.pformat().splitlines()]
-    ) == "\n".join([ll.rstrip() for ll in expected.splitlines()])
+@with_parameters(FIXTURE_PATH / "tables.md")
+def test_tables(file_params):
+    document = to_docutils(file_params.content, in_sphinx_env=True)
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("directive_options.md")),
-    ids=[
-        f"{i[0]}-{i[1]}"
-        for i in read_fixture_file(FIXTURE_PATH / "directive_options.md")
-    ],
-)
-def test_directive_options(line, title, input, expected):
-    document = to_docutils(input)
-    print(document.pformat())
-    assert "\n".join(
-        [ll.rstrip() for ll in document.pformat().splitlines()]
-    ) == "\n".join([ll.rstrip() for ll in expected.splitlines()])
+@with_parameters(FIXTURE_PATH / "directive_options.md")
+def test_directive_options(file_params):
+    document = to_docutils(file_params.content)
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("sphinx_directives.md")),
-    ids=[
-        f"{i[0]}-{i[1]}"
-        for i in read_fixture_file(FIXTURE_PATH / "sphinx_directives.md")
-    ],
-)
-def test_sphinx_directives(line, title, input, expected):
+@with_parameters(FIXTURE_PATH / "sphinx_directives.md")
+def test_sphinx_directives(file_params):
     # TODO fix skipped directives
     # TODO test domain directives
-    if title.startswith("SKIP"):
-        pytest.skip(title)
-    elif title.startswith("SPHINX3") and sphinx.version_info[0] < 3:
-        pytest.skip(title)
-    elif title.startswith("SPHINX4") and sphinx.version_info[0] < 4:
-        pytest.skip(title)
-    document = to_docutils(input, in_sphinx_env=True)
-    _actual, _expected = [
-        "\n".join([ll.rstrip() for ll in text.splitlines()])
-        for text in (document.pformat(), expected)
-    ]
-    try:
-        assert _actual == _expected
-    except AssertionError:
-        print(document.pformat())
-        raise
+    if file_params.title.startswith("SKIP"):
+        pytest.skip(file_params.title)
+    elif file_params.title.startswith("SPHINX3") and sphinx.version_info[0] < 3:
+        pytest.skip(file_params.title)
+    elif file_params.title.startswith("SPHINX4") and sphinx.version_info[0] < 4:
+        pytest.skip(file_params.title)
+    document = to_docutils(file_params.content, in_sphinx_env=True)
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("sphinx_roles.md")),
-    ids=[f"{i[0]}-{i[1]}" for i in read_fixture_file(FIXTURE_PATH / "sphinx_roles.md")],
-)
-def test_sphinx_roles(line, title, input, expected):
-    if title.startswith("SKIP"):
-        pytest.skip(title)
-    elif title.startswith("SPHINX4") and sphinx.version_info[0] < 4:
-        pytest.skip(title)
-    document = to_docutils(input, in_sphinx_env=True)
-    print(document.pformat())
-    _actual, _expected = [
-        "\n".join([ll.rstrip() for ll in text.splitlines()])
-        for text in (document.pformat(), expected)
-    ]
+@with_parameters(FIXTURE_PATH / "sphinx_roles.md")
+def test_sphinx_roles(file_params):
+    if file_params.title.startswith("SKIP"):
+        pytest.skip(file_params.title)
+    elif file_params.title.startswith("SPHINX4") and sphinx.version_info[0] < 4:
+        pytest.skip(file_params.title)
+    document = to_docutils(file_params.content, in_sphinx_env=True)
+    actual = document.pformat()
     # sphinx 3 adds a parent key
-    _actual = re.sub('cpp:parent_key="[^"]*"', 'cpp:parent_key=""', _actual)
-    assert _actual == _expected
+    actual = re.sub('cpp:parent_key="[^"]*"', 'cpp:parent_key=""', actual)
+    file_params.assert_expected(actual, rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("amsmath.md")),
-    ids=[f"{i[0]}-{i[1]}" for i in read_fixture_file(FIXTURE_PATH / "amsmath.md")],
-)
-def test_amsmath(line, title, input, expected, monkeypatch):
+@with_parameters(FIXTURE_PATH / "amsmath.md")
+def test_amsmath(file_params, monkeypatch):
     monkeypatch.setattr(SphinxRenderer, "_random_label", lambda self: "mock-uuid")
     document = to_docutils(
-        input, MdParserConfig(enable_extensions=["amsmath"]), in_sphinx_env=True
+        file_params.content,
+        MdParserConfig(enable_extensions=["amsmath"]),
+        in_sphinx_env=True,
     )
-    print(document.pformat())
-    _actual, _expected = [
-        "\n".join([ll.rstrip() for ll in text.splitlines()])
-        for text in (document.pformat(), expected)
-    ]
-    assert _actual == _expected
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("containers.md")),
-    ids=[f"{i[0]}-{i[1]}" for i in read_fixture_file(FIXTURE_PATH / "containers.md")],
-)
-def test_containers(line, title, input, expected, monkeypatch):
+@with_parameters(FIXTURE_PATH / "containers.md")
+def test_containers(file_params, monkeypatch):
     monkeypatch.setattr(SphinxRenderer, "_random_label", lambda self: "mock-uuid")
     document = to_docutils(
-        input, MdParserConfig(enable_extensions=["colon_fence"]), in_sphinx_env=True
+        file_params.content,
+        MdParserConfig(enable_extensions=["colon_fence"]),
+        in_sphinx_env=True,
     )
-    print(document.pformat())
-    _actual, _expected = [
-        "\n".join([ll.rstrip() for ll in text.splitlines()])
-        for text in (document.pformat(), expected)
-    ]
-    assert _actual == _expected
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("eval_rst.md")),
-    ids=[f"{i[0]}-{i[1]}" for i in read_fixture_file(FIXTURE_PATH / "eval_rst.md")],
-)
-def test_evalrst_elements(line, title, input, expected):
-    document = to_docutils(input, in_sphinx_env=True)
-    print(document.pformat())
-    assert "\n".join(
-        [ll.rstrip() for ll in document.pformat().splitlines()]
-    ) == "\n".join([ll.rstrip() for ll in expected.splitlines()])
+@with_parameters(FIXTURE_PATH / "eval_rst.md")
+def test_evalrst_elements(file_params):
+    document = to_docutils(file_params.content, in_sphinx_env=True)
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("definition_lists.md")),
-    ids=[
-        f"{i[0]}-{i[1]}"
-        for i in read_fixture_file(FIXTURE_PATH / "definition_lists.md")
-    ],
-)
-def test_definition_lists(line, title, input, expected):
+@with_parameters(FIXTURE_PATH / "definition_lists.md")
+def test_definition_lists(file_params):
     document = to_docutils(
-        input, MdParserConfig(enable_extensions=["deflist"]), in_sphinx_env=True
+        file_params.content,
+        MdParserConfig(enable_extensions=["deflist"]),
+        in_sphinx_env=True,
     )
-    print(document.pformat())
-    assert "\n".join(
-        [ll.rstrip() for ll in document.pformat().splitlines()]
-    ) == "\n".join([ll.rstrip() for ll in expected.splitlines()])
+    file_params.assert_expected(document.pformat(), rstrip_lines=True)
--- tests/test_renderers/test_include_directive.py
+++ tests/test_renderers/test_include_directive.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import pytest
-from markdown_it.utils import read_fixture_file
+from pytest_param_files import with_parameters
 
 from myst_parser.docutils_renderer import make_document
 from myst_parser.main import to_docutils
@@ -10,26 +10,21 @@ from myst_parser.main import to_docutils
 FIXTURE_PATH = Path(__file__).parent.joinpath("fixtures")
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("mock_include.md")),
-)
-def test_render(line, title, input, expected, tmp_path):
+@with_parameters(FIXTURE_PATH / "mock_include.md")
+def test_render(file_params, tmp_path):
     tmp_path.joinpath("other.md").write_text("a\nb\nc")
     tmp_path.joinpath("fmatter.md").write_text("---\na: 1\n---\nb")
     document = make_document(str(tmp_path / "test.md"))
-    to_docutils(input, document=document, in_sphinx_env=True, srcdir=str(tmp_path))
+    to_docutils(
+        file_params.content, document=document, in_sphinx_env=True, srcdir=str(tmp_path)
+    )
     output = document.pformat().replace(str(tmp_path) + os.sep, "tmpdir" + "/").rstrip()
-    print(output)
-    assert output == expected.rstrip()
+    file_params.assert_expected(output, rstrip=True)
 
 
-@pytest.mark.parametrize(
-    "line,title,input,expected",
-    read_fixture_file(FIXTURE_PATH.joinpath("mock_include_errors.md")),
-)
-def test_errors(line, title, input, expected, tmp_path):
-    if title.startswith("Non-existent path") and os.name == "nt":
+@with_parameters(FIXTURE_PATH / "mock_include_errors.md")
+def test_errors(file_params, tmp_path):
+    if file_params.title.startswith("Non-existent path") and os.name == "nt":
         pytest.skip("tmp_path not converted correctly on Windows")
 
     tmp_path.joinpath("bad.md").write_text("{a}`b`")
@@ -44,5 +39,7 @@ def test_errors(line, title, input, expected, tmp_path):
 
     document.reporter.attach_observer(observer)
     document.reporter.halt_level = 6
-    to_docutils(input, document=document, in_sphinx_env=True, srcdir=str(tmp_path))
-    assert "\n".join(messages).rstrip() == expected.rstrip()
+    to_docutils(
+        file_params.content, document=document, in_sphinx_env=True, srcdir=str(tmp_path)
+    )
+    file_params.assert_expected("\n".join(messages), rstrip=True)
