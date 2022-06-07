fa164012049eb415ab4ba37aa0ba49a8316d11e0
Chris Sewell <chrisj_sewell@hotmail.com>
2022-06-07 13:47:16 +0200

    🐛 FIX: `parse_directive_text` when body followed by options

--- tests/test_renderers/test_parse_directives.py
+++ tests/test_renderers/test_parse_directives.py
@@ -1,27 +1,59 @@
-# TODO add more tests
+from pathlib import Path
+
 import pytest
-from docutils.parsers.rst.directives.admonitions import Note
+import yaml
+from docutils.parsers.rst.directives.admonitions import Admonition, Note
 from docutils.parsers.rst.directives.body import Rubric
+from markdown_it import MarkdownIt
 
 from myst_parser.parsers.directives import DirectiveParsingError, parse_directive_text
 
+FIXTURE_PATH = Path(__file__).parent.joinpath("fixtures")
 
-@pytest.mark.parametrize(
-    "klass,arguments,content",
-    [(Note, "", "a"), (Note, "a", ""), (Note, "", ":class: name\n\na")],
-)
-def test_parsing(klass, arguments, content, data_regression):
+
+@pytest.mark.param_file(FIXTURE_PATH / "directive_parsing.txt")
+def test_parsing(file_params):
+    """Test parsing of directive text."""
+    tokens = MarkdownIt("commonmark").parse(file_params.content)
+    assert len(tokens) == 1 and tokens[0].type == "fence"
+    name, *first_line = tokens[0].info.split(maxsplit=1)
+    if name == "{note}":
+        klass = Note
+    elif name == "{admonition}":
+        klass = Admonition
+    else:
+        raise AssertionError(f"Unknown directive: {name}")
     arguments, options, body_lines, content_offset = parse_directive_text(
-        klass, arguments, content
+        klass, first_line[0] if first_line else "", tokens[0].content
     )
-    data_regression.check(
+    outcome = yaml.safe_dump(
         {
             "arguments": arguments,
             "options": options,
             "body": body_lines,
             "content_offset": content_offset,
-        }
+        },
+        sort_keys=True,
     )
+    file_params.assert_expected(outcome, rstrip_lines=True)
+
+
+# @pytest.mark.parametrize(
+#     "klass,arguments,content",
+#     [(Note, "", "a"), (Note, "a", ""), (Note, "", ":class: name\n\na")],
+# )
+# def test_parsing(klass, arguments, content, data_regression):
+#     arguments, options, body_lines, content_offset = parse_directive_text(
+#         klass, arguments, content
+#     )
+#     data_regression.check(
+#         {
+#             "arguments": arguments,
+#             "options": options,
+#             "body": body_lines,
+#             "content_offset": content_offset,
+#         }
+#     )
 
 
 @pytest.mark.parametrize(
