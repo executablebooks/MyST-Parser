4765b09c55fb5fbd1a777d4facab9c7c0756235e
Chris Sewell <chrisj_sewell@hotmail.com>
2022-12-13 18:18:28 +0100

    more tests

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -3,19 +3,31 @@ from pathlib import Path
 
 import pytest
 
-from myst_parser.inventory import filter_inventories, inventory_cli, load, to_sphinx
+from myst_parser.inventory import (
+    filter_inventories,
+    from_sphinx,
+    inventory_cli,
+    load,
+    to_sphinx,
+)
 
 STATIC = Path(__file__).parent.absolute() / "static"
 
 
-def test_inv_resolve(data_regression):
+def test_convert_roundtrip():
+    with (STATIC / "objects_v2.inv").open("rb") as f:
+        inv = load(f)
+    assert inv == from_sphinx(to_sphinx(inv))
+
+
+def test_inv_filter(data_regression):
     with (STATIC / "objects_v2.inv").open("rb") as f:
         inv = to_sphinx(load(f))
     output = [m.asdict() for m in filter_inventories({"inv": inv}, "index")]
     data_regression.check(output)
 
 
-def test_inv_resolve_fnmatch(data_regression):
+def test_inv_filter_fnmatch(data_regression):
     with (STATIC / "objects_v2.inv").open("rb") as f:
         inv = to_sphinx(load(f))
     output = [
